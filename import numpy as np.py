17ad0e8bfc81d4685ab403b952f0713b0bb9c927
Alexey Zhivin <aleksey_zhivin@mail.ru>
2023-12-15 02:02:16 +0300

    awef hqf;oijcre

--- import numpy as np.py
+++ import numpy as np.py
@@ -1,9 +1,6 @@
 import numpy as np
 import scipy.optimize as opt
 import matplotlib
-import matplotlib.pyplot as plt
-import pandas as pd
-from colorsys import hsv_to_rgb as hsv
 
 def line(x, a, b):
     return a + b * x
