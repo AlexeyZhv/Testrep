1acc50b94bda7cd5080762fc44758b5af74aa4c9
LidiaReb <81241178+LidiaReb@users.noreply.github.com>
2023-12-15 02:06:44 +0300

    dfk

--- import numpy as np.py
+++ import numpy as np.py
@@ -1,9 +1,9 @@
 import numpy as np
-import scipy.optimize as opt
-import matplotlib
+import matplotlib as pltiti
 import matplotlib.pyplot as plt
 import pandas
 from colorsys import hsv_to_rgb as hsv
+import math
 
 def line(x, a, b):
     return a + b * x + 29
@@ -11,5 +11,7 @@ def line(x, a, b):
 font = {'family' : 'normal',
         'weight' : 'normal',
         'size'   : 14}
+
+print("jflakfjeke?")
 
 matplotlib.rc('font', **font)
