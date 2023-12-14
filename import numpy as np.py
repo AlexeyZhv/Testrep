502cfd860357368730f75dc7b8a5916f5390235d
LidiaReb <81241178+LidiaReb@users.noreply.github.com>
2023-12-15 02:02:49 +0300

    dfk

--- import numpy as np.py
+++ import numpy as np.py
@@ -2,11 +2,11 @@ import numpy as np
 import scipy.optimize as opt
 import matplotlib
 import matplotlib.pyplot as plt
-import pandas as pd
+import pandas
 from colorsys import hsv_to_rgb as hsv
 
 def line(x, a, b):
-    return a + b * x
+    return a + b * x + 29
 
 font = {'family' : 'normal',
         'weight' : 'normal',
